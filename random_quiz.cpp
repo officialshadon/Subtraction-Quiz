61d4eda240dca94e8ff9b314db66738df47f6e89
Nnaemeka Onyeokoro <69983700+officialshadon@users.noreply.github.com>
2021-02-13 13:02:40 -0800

    Update random_quiz.cpp
    I literally updated the whole code. 
    This doesn't mean I changed a lot. I just noticed a few bugs and decided to
    copy and paste better code( hopefully).

--- random_quiz.cpp
+++ random_quiz.cpp
@@ -7,17 +7,18 @@ using namespace std;
 int main()
 {
 	// Generate two random numbers
-	srand(time(0));			 // Makes sure the seed is never the same
-	int num1, num2;
-	num1 = rand() % 11; 	// make sure the number is between 0 and 10...inclusive
-	num2 = rand() % 11;
+
 	int correct;
 	int answer;
 	// Let's put it in a loop
 	int numOfTimes;
-	cout << "How many tests will you like to try? << '\n';
+	cout << "How many tests will you like to try?" << '\n';
 	cin >> numOfTimes;
 	for ( int i = 0; i < numOfTimes; ++i ){
+		srand(time(0));			 // Makes sure the seed is never the same
+		int num1, num2;
+		num1 = rand() % 11; 	// make sure the number is between 0 and 10...inclusive
+		num2 = rand() % 11;
 		if ( num1 >= num2){
 			cout << "What is " << num1 << " - "<< num2 << '?' << '\n';
 			cin >> answer;	
@@ -27,7 +28,6 @@ int main()
 			cin >> answer;
 			correct = num2 - num1;
 		}
-
 		if (answer == correct){
 			cout << "Good job, you got it right" << '\n';
 		} else{
