7853b0378ad5167702234cfaef93fb05af55ee46
Nnaemeka Onyeokoro <69983700+officialshadon@users.noreply.github.com>
2021-02-12 21:02:39 -0800

    Update random_quiz.cpp
    I made this subtraction quiz ask the user for the amount of times they'd like to do the test.
    So I added a for loop.

--- random_quiz.cpp
+++ random_quiz.cpp
@@ -13,21 +13,26 @@ int main()
 	num2 = rand() % 11;
 	int correct;
 	int answer;
+	// Let's put it in a loop
+	int numOfTimes;
+	cout << "How many tests will you like to try? << '\n';
+	cin >> numOfTimes;
+	for ( int i = 0; i < numOfTimes; ++i){
+		if ( num1 >= num2){
+			cout << "What is " << num1 << " - "<< num2 << '?' << '\n';
+			cin >> answer;	
+			correct = num1 - num2;
+		}else{
+			cout << "What is " << num2 << " - " << num1 << '?' << '\n';
+			cin >> answer;
+			correct = num2 - num1;
+		}
 
-	if ( num1 >= num2){
-		cout << "What is " << num1 << " - "<< num2 << '?' << '\n';
-		cin >> answer;	
-		correct = num1 - num2;
-	}else{
-		cout << "What is " << num2 << " - " << num1 << '?' << '\n';
-		cin >> answer;
-		correct = num2 - num1;
-	}
-
-	if (answer == correct){
-		cout << "Good job, you got it right" << '\n';
-	} else{
-		cout << "You didn't quite get it right but good job still!!" << '\n';
+		if (answer == correct){
+			cout << "Good job, you got it right" << '\n';
+		} else{
+			cout << "You didn't quite get it right but good job still!!" << '\n';
+		}
 	}
 	return 0;
 }
